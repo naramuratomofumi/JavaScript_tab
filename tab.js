8b7bda1b7f7b12953b9d21c8c17625561cdf0353
naramuratomofumi <tomofuminaramura@gmail.com>
2022-02-04 19:55:35 +0900

    is-activeをリファクタリング

--- tab.js
+++ tab.js
@@ -4,6 +4,7 @@
   const $tab = $doc.getElementById("js-tab");
   const $nav = $tab.querySelectorAll('[data-nav]');
   const $content = $tab.querySelectorAll('[data-content]');
+  const ACTIVE_CLASS = 'is-active';
 
   // 初期化
   const init = () => {
@@ -24,13 +25,13 @@
     let index = 0
     while(index < $nav.length){
       $content[index].style.display = 'none';
-      $nav[index].classList.remove('is-active');
+      $nav[index].classList.remove(ACTIVE_CLASS);
       index++;
     }
 
     // 対象のコンテンツをアクティブ化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
-    $nav[targetVal].classList.add('is-active');
+    $nav[targetVal].classList.add(ACTIVE_CLASS);
   };
   // 全nav要素に対して関数を適応・発火
   let index = 0;
