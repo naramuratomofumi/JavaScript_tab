1d96cac64b9015fc46b144d7d9c4ddb51383c2f5
naramuratomofumi <tomofuminaramura@gmail.com>
2022-02-04 19:51:58 +0900

    タブをクリックすると一旦リセットするように設定

--- tab.js
+++ tab.js
@@ -20,11 +20,19 @@
     const targetVal = $this.dataset.nav;
     console.log('targetVal', targetVal);
 
+    // 対象外のnav、contentを全て一旦リセットする
+    let index = 0
+    while(index < $nav.length){
+      $content[index].style.display = 'none';
+      $nav[index].classList.remove('is-active');
+      index++;
+    }
+
     // 対象のコンテンツをアクティブ化する
     $tab.querySelectorAll('[data-content="' + targetVal + '"]')[0].style.display = 'block';
-    $nav[targetVal].classList.add('is-active')
+    $nav[targetVal].classList.add('is-active');
   };
-// 全nav要素に対して関数を適応・発火
+  // 全nav要素に対して関数を適応・発火
   let index = 0;
   while(index < $nav.length){
     $nav[index].addEventListener('click', (e) => handleClick(e));
